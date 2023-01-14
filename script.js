8fea2c2a8e17338ff2e62fd5113469b5a9605d2a
itsChix <64463196+itsChix@users.noreply.github.com>
2023-01-14 22:06:30 +0100

    Animacje counterow

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { CountUp } from './data/js/countUp.min.js';
+
 let counter = 0;
 let text = document.getElementById("changeText");
 let progressbar = document.getElementById("resetprogress")
@@ -16,18 +18,37 @@ function change() {
   }
 }
 
+
+let number = 2;
+let previous_data = {'temperature': 0, 'wind_speed_kmh': 0, 'pressure': 0, 'rain_count': 0, 'humidity' : 0}; // dla counterów
+
 function requestData(){
+  if(number > 5){
+    number = 2;
+  }
   $.ajax({
     url: ".db/sqlconnect.php",
     type: 'POST',
+    data: {"num": number},
     dataType:"json",
     success: function(data) {
-      document.getElementById("temperature-value").innerText = data['temperature'];
-      document.getElementById("wind-value").innerText = data['wind_speed_kmh'];
-      document.getElementById("pressure-value").innerText = data['pressure'];
-      document.getElementById("rain-value").innerText = data['rain_count'];
-      document.getElementById("humidity-value").innerText = data['humidity'];
+      var countTemp = new CountUp('temperature-value', data['temperature'], {startVal: previous_data['temperature'], decimalPlaces: 2, duration: 2});
+      var countWind = new CountUp('wind-value', data['wind_speed_kmh'], {startVal: previous_data['wind_speed_kmh'], decimalPlaces: 2, duration: 2});
+      var countPressure = new CountUp('pressure-value', data['pressure'], {startVal: previous_data['pressure'], useGrouping: false, duration: 2});
+      var countRain = new CountUp('rain-value', data['rain_count'], {startVal: previous_data['rain_count'], decimalPlaces: 2, duration: 2});
+      var countHumidity = new CountUp('humidity-value', data['humidity'], {startVal: previous_data['humidity'], duration: 2});
+      
+      countTemp.start();
+      countWind.start();
+      countPressure.start();
+      countRain.start();
+      countHumidity.start();
+      
+      previous_data = data;
+
    }})
+   
+   number += 1;
 }
 
 requestData();
