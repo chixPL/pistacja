62dc717ad7acd6ddf56eb6653bd7e1e55fba7379
itsChix <64463196+itsChix@users.noreply.github.com>
2023-01-14 21:17:18 +0100

    Refactor do AJAX

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let counter = 0;
 let text = document.getElementById("changeText");
 let progressbar = document.getElementById("resetprogress")
-let inst = setInterval(change, 1000);
+let progress = setInterval(change, 1000);
+let updateSql = setInterval(requestData, 5000)
 
 function change() {
   text.innerHTML = "Restart za " + (5-counter) + " sekund";
@@ -13,8 +14,23 @@ function change() {
   if(counter > 5){
     counter = 0;
   }
-
 }
+
+function requestData(){
+  $.ajax({
+    url: ".db/sqlconnect.php",
+    type: 'POST',
+    dataType:"json",
+    success: function(data) {
+      document.getElementById("temperature-value").innerText = data['temperature'];
+      document.getElementById("wind-value").innerText = data['wind_speed_kmh'];
+      document.getElementById("pressure-value").innerText = data['pressure'];
+      document.getElementById("rain-value").innerText = data['rain_count'];
+      document.getElementById("humidity-value").innerText = data['humidity'];
+   }})
+}
+
+requestData();
 
 /*
 setTimeout(function(){
