755acb78997e7fc4b9feb8172d5136b58591e94b
itsChix <64463196+itsChix@users.noreply.github.com>
2023-01-14 18:07:54 +0100

    Wszystkie wartosci z bazy danych

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 include('.db/sqlconnect.php');
-$sql = "SELECT temperature_1 FROM results WHERE id_measure = 1";
-$rs = mysqli_query($conn, $sql);
+$sql = "SELECT wind_speed_kmh, rain_count, ROUND(((temperature_1 + temperature_2) /2),2) AS temperature, ROUND(pressure, 0) AS pressure, humidity, wind_gust_kmh FROM results WHERE id_measure = 2";
+$query = mysqli_query($conn, $sql);
+$rs= mysqli_fetch_array($query);
 
 ?>
 
@@ -12,7 +13,8 @@ $rs = mysqli_query($conn, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Pistacja</title>
 </head>
@@ -30,40 +32,51 @@ $rs = mysqli_query($conn, $sql);
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="#">Strona główna</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+                <a class="nav-link" href="https://github.com/chixPL/pistacja-frontend">GitHub</a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
 
+      <div class="container-fluid mt-1">
+        <!-- Wyniki z DB -->
         <div class="container ms-1">
         <h1>Temperatura:
-        <h2><?php echo $rs->fetch_array()[0];?> °C</h2>
+        <h2><b><?php echo $rs['temperature']?><span class="unit superscript"> °C</span></b></h2>
         </div>
+        <br>
 
-        <p id="changeText"></p>
-        <script src="script.js"></script>
+        <div class="container ms-1">
+        <h1>Wiatr:
+        <h2><b><?php echo $rs['wind_speed_kmh'];?><span class="unit"> km/h</span></b></h2>
+        </div>
+        <br>
+
+        <div class="container ms-1">
+        <h1>Ciśnienie:
+        <h2><b><?php echo $rs['pressure'];?><span class="unit"> hPa</span></b></h2>
+        </div>
+        <br>
+
+        <div class="container ms-1">
+        <h1>Deszcz:
+        <h2><b><?php echo $rs['rain_count'];?><span class="unit"> mm</span></b></h2>
+        </div>
+        <br>
+
+        <div class="container ms-1">
+        <h1>Wilgotność:
+        <h2><b><?php echo $rs['humidity'];?><span class="unit"> %</span></b></h2>
+        </div>
+        <br>
+
+      </div>
+
+      <p id="changeText"></p>
+      <script src="script.js"></script>
 </body>
 </html>
