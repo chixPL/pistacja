0c5ebbc0222601bbe8474c794d9a9e126a552b0a
itsChix <64463196+itsChix@users.noreply.github.com>
2023-02-20 16:51:28 +0100

    Ulepszony push

--- push.sh
+++ push.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-rsync -r --exclude=.git /run/user/1001/gvfs/ftp:host=losto.net,user=pogoda%40losto.net/* /home/spolek/temp
-cd /home/spolek/temp
+repo_dir = $(pwd)
+if [[ "$repo_dir" == *"pogoda"* ]]; then
+  $target_dir = $HOME/temp/pogoda
+elif [[ "$repo_dir" == *"g1"* ]]; then
+  $target_dir = $HOME/temp/g1
+fi
+rsync -r --update --exclude=.git $repo_dir/* $target_dir
+cd $target_dir
 git add .
 git commit -m "$1"
 git push
