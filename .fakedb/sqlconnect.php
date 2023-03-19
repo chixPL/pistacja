d70220c6bc5d69e974e65adb689cb006b6df9937
itsChix <64463196+itsChix@users.noreply.github.com>
2023-03-19 12:32:02 +0100

    Update db

--- .fakedb/sqlconnect.php
+++ .fakedb/sqlconnect.php
@@ -7,7 +7,7 @@ $password = "my_password";
 $port = 3306;
 
 $conn = mysqli_connect($host, $user, $password, $database, $port);
-$sql = "SELECT wind_speed_kmh, rain_count, ROUND(((temperature_1 + temperature_2) /2),2) AS temperature, ROUND(pressure, 0) AS pressure, humidity, wind_gust_kmh FROM results WHERE id_measure = 2";
+$sql = sprintf("SELECT wind_speed_kmh, rain_count, ROUND(((temperature_1 + temperature_2) /2),2) AS temperature, ROUND(pressure, 0) AS pressure, humidity, wind_gust_kmh FROM results WHERE id_measure = %d", $_POST['num']);
 $query = mysqli_query($conn, $sql);
 $rs= mysqli_fetch_array($query);
 
